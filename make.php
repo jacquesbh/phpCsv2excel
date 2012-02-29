8434a81f2f15fd9f3bb6aa70b5235d9484b73936
Jacques Bodin-Hullin <jacques@joliebox.com>
2012-02-29 10:58:26 +0100

    Rename project from phpCsv2xml to phpCsv2excel

--- make.php
+++ make.php
@@ -1,5 +1,5 @@
 <?php
-$p = new Phar('phpCsv2xml.phar');
+$p = new Phar('phpCsv2excel.phar');
 $p->startBuffering();
-$p->buildFromDirectory('./phpCsv2xml');
+$p->buildFromDirectory('./phpCsv2excel');
 $p->stopBuffering();
